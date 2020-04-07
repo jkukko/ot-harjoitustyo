487f95b4044a9c7af84b1986326cd5e7f310f81b
jkukko <joonas.kukkonen@gmail.com>
2020-04-07 20:27:18 +0300

    Mahdollisuus valita kumpaa käyttöliittymää käyttää

--- InventoryManagement/src/main/java/inventoryManagement/Main.java
+++ InventoryManagement/src/main/java/inventoryManagement/Main.java
@@ -8,11 +8,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /*Scanner scanner = new Scanner(System.in);
-        TextUi ui = new TextUi(scanner);
-        ui.start();*/
-        GraphicUi ui = new GraphicUi();
-        ui.main(args);
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        
+        System.out.println("Valitse Käyttöliittymä");
+        System.out.println("1 tekstikäyttöliittymä");
+        System.out.println("Muuten graafinenkäyttöliittymä");
+        String valinta = scanner.nextLine();
+        if (valinta.equals("1")) {
+            TextUi ui = new TextUi(scanner);
+            ui.start();            
+        } else {
+            GraphicUi ui = new GraphicUi();
+            ui.main(args);
+        }
+        
     }
     
 }
